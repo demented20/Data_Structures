1da58246df0f31507894bf318691102388ee4581
demented20 <younessmadani13@gmail.com>
2026-08-14 03:01:58 +0800

    i fixed the errors with the vector and made the stuctures for the hashmap

--- data_structures.c
+++ data_structures.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
-#include<stdlib.h>
+#include "data_structures.h"
+
+#ifndef DATA_STRUCTURES__H
+#define DATA_STRUCTURES__H
+
+// YOU CAN ENABLE THE AUTOMATIC VECTOR RESIZE FEATURE BY UNCOMMENTING THE NEXT COMMAND 
+//#define FREE_UNNECESSARY_CAPACITY
+
+
 
 // int linked list definition 
-
-typedef struct Node {
-    int data;
-    struct Node* next;
-} Node;
 
 Node* pushtoll(Node* head,int val){
     if (head == NULL) {
@@ -25,12 +27,6 @@ Node* pushtoll(Node* head,int val){
     }
 } 
 // int stack implementation and member function defintion 
-typedef struct stack
-{
-    Node *head;
-    int node_count;
-
-}stack;
 // operations : inti()/top()/push()/pop()/size()/empty()
 stack * stk_init(){
     stack * newStack = malloc(sizeof(stack));
@@ -67,14 +63,6 @@ int stk_top(stack * stk){
     if(stk->node_count == 0) return -1;
     else return stk->head->data;
 }
-
-// int queue implementation and member function defintion 
-typedef struct queue
-{
-    Node *head , *tail;
-    int node_count;
-    
-}queue;
 
 queue * queue_init(){
     queue * newQueue = malloc(sizeof(queue));
@@ -124,13 +112,104 @@ int queue_empty(queue * q){
     if(q->node_count == 0) return 1;
     else return 0;
 }
+//defining and implementing dynamic array methods 
+Header * vector_init(int init_capacity){
+    if (init_capacity <= 0)
+        init_capacity= MIN_CAPACITY;
+    Header * head = malloc(sizeof(Header)+init_capacity* sizeof(int));
+    if (head == NULL) return NULL;// failed to init the vector
+    head->capacity = init_capacity;
+    head->count = 0;
+    head->items = (int*)(head+1);
+    return head;
+}
+Header* vector_push(Header *pointer_to_header , int element_tobe_pushed){
+    if(pointer_to_header == NULL) return NULL;//either the pointer doesnt exist or the array is un initialized 
+    if(pointer_to_header->capacity >= pointer_to_header->count+1) goto directly_push;
+    else {
+        // we need to realloc anew chunk with more capacity 
+        size_t new_capacity = pointer_to_header->capacity * 2;
+        Header  *new_header = realloc(pointer_to_header, sizeof(Header) + sizeof(int) * new_capacity);
+        if (new_header == NULL) return NULL;// the reallocation failed   
+            //we update the meta data       
+            new_header->capacity = new_capacity;
+            new_header->items= (int*)(new_header+ 1);
+            pointer_to_header = new_header;
+    }  
+    directly_push :
+    ((int*)(pointer_to_header+1))[pointer_to_header->count++] = element_tobe_pushed;
+    return pointer_to_header;
+} 
+int get_length(Header* meta_data){
+        if (meta_data == NULL) return -1 ;//for failure 
+        return meta_data->count;
+}
+int get_capacity(Header * meta_data){
+        if (meta_data == NULL) return -1;//for failure 
+        return meta_data->capacity;
+}
+void free_the_array(Header *meta_data){
+        if (meta_data ==NULL) return;
+        free(meta_data);
+}
 
+Header* array_delete_element(Header* meta_data,int element){
+        if (meta_data==NULL) return NULL; //handles invalid pointer 
+        if (meta_data->count == 0 ) return meta_data;// handles empty arrays by returning the original empty vector
+
+        int index = -1; //way of simulating the boolean and at the same time returning the index if student id is found found
+        for (size_t i = 0; i < meta_data->count; i++){
+
+            if ((meta_data->items)[i] == element){
+                index = i;//recording the index of the element we want to delete
+                break;
+            }
+        } 
+        if (index == -1) return meta_data;
+                //swwapping with the last element
+                //its more of a overwrite than a complete swap 
+                if (index != meta_data->count - 1) (meta_data->items)[index] = (meta_data->items)[meta_data->count - 1];
+                //upadate meta data 
+                meta_data->count--;
+
+                //this part of the code is added to shrink if the count of the elements is less than the half of the array capacity
+                // this is a good way to keep your memory usage minimal and ur code optimized 
+                //it can be omited by undefining or commenting it
+                #ifdef FREE_UNNECESSARY_CAPACITY
+
+                if (meta_data->count < (meta_data->capacity / 2) &&( meta_data->capacity > MIN_CAPACITY)){
+
+                    size_t new_capacity =meta_data->capacity / 2;
+                    if (new_capacity< MIN_CAPACITY) new_capacity = MIN_CAPACITY;
+                    //this allows us to handle the array with few elements ,
+                    //because we increase the capacity be x1.5 (and this might cause problems when it comes to 0 and 1 elements and we want to push)
+                    //this way we dont allow the array to go bellow 32 elements        
+                    //__________________________________________________________________________________________________________________________________
+                    
+                    //these following lines reallocate memory for an array using the pointer in the parameters and the new size (half of the precious size)
+                    Header *new_vector = realloc(meta_data,sizeof(Header) +sizeof(int) *new_capacity);
+
+                    if (new_vector == NULL) return meta_data; //if the operation fails we still have the pointer to the metadata
+                    //meta data update
+                    new_vector->capacity = new_capacity;
+                    new_vector->items = (int*)(new_vector+ 1);
+                    return new_vector;
+                }
+                #endif
+
+            return meta_data;
+        
+    }
+
+//now the inmplementatios of the min/max heap using our recently created dynamic array or vector 
+
+#endif
 int main (){
     printf("the code works at least \n");
     queue * new_q = queue_init();
     if(new_q) printf("the queue is created succesfully \n");
     stack * new_stk = stk_init();
     if(new_stk) printf("the stack is created succesfully ");
-
-    return 0;
+    void *newvec = vector_init(256);
+    return 0; 
 }
--- data_structures.h
+++ data_structures.h
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include<stdlib.h>
+#define INIT_CAPACITY 256
+#define MIN_CAPACITY 32
 
 typedef struct Node {
     int data;
@@ -22,8 +24,8 @@ void stk_push(stack * stk, int val);
 void stk_pop(stack * stk);
 int stk_empty(stack * stk);
 int stk_top(stack * stk);
-
-typedef struct
+// Queue data structure
+typedef struct queue
 {
     Node *head , *tail;
     int node_count;
@@ -36,4 +38,42 @@ void queue_push(queue * q, int val);
 int queue_front(queue * q);
 void queue_pop(queue * q);
 int queue_empty(queue * q);
-
+//hashmap data structure
+typedef struct Header
+{
+    int *items; // this is pointer to the first int element 
+    int capacity; // this is the max number of integers that the vector can store
+    int count ; // this holds the count of the integer elements that are storred in the vector
+}Header;
+//Member Methods :
+Header * vector_init(int init_capacity);
+Header* vector_push(int *pointer_to_first_element , int element_tobe_pushed);
+int get_length(Header * meta_data);
+int get_capacity(Header *meta_data);
+Header* vector_delete_element(Header* meta_data,int element); 
+void free_the_vector(Header* meta_data);
+//Hashmap node structure
+typedef struct Bucket
+{
+    HashNode * head;
+    int bucket_index;
+}Bucket;
+typedef struct HashmapHeader
+{
+    Bucket *buckets; // this is pointer to the first bucket
+    int capacity; // this is the max number of buckets that the vector can store
+    int count ; // this holds the count of the buckets that are storred in the vector
+}HashmapHeader;
+typedef struct HashNode
+{
+    int key;
+    int value;
+    HashNode * next;
+}HashNode;
+//Member Methods :
+HashmapHeader * hashMap_init(int initial_buckets);
+void hashMap_insert(HashmapHeader* hashmap, int key , int val);
+int hashMap_get(HashmapHeader * hashmap, const int key);
+int hashMap_exists(HashmapHeader * hashmap, const int key);
+void hashmap_remove(HashmapHeader * hashmap, const int key);
+void hashMap_destroy(HashmapHeader * hashmap);
